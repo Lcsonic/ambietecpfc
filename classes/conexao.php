4fc3a3e165232e1218f056cb154168246d207717
MatheusDLM <matheus.maia100@hotmail.com>
2019-05-24 21:21:56 -0300

    tentativa 1

--- classes/conexao.php
+++ classes/conexao.php
@@ -5,6 +5,12 @@
 	$dbname = "green_alert"; //nome do banco
 	
 	//Criar a conexão
-	$conexao = mysqli_connect($servidor, $usuario, $senha, $dbname);
+	$conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
+
+	if(!$conn){
+        die("Falha na conexao: " . mysqli_connect_error());
+    }else{
+        echo "Conexao realizada com sucesso";
+    }   
 
 ?>
--- index.php
+++ index.php
@@ -1,55 +1,70 @@
-
-
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Menu Inicial</title>  
+  <title>Menu Inicial</title>
 
   <!-- Imports !-->
-  <link rel="stylesheet" href="./css/css-index.css">   
+  <link rel="stylesheet" href="./css/css-index.css">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
+
 <body>
   <div class="container">
 
-      
 
-        <div class="logoTxt">
-          Green Alert
+
+    <div class="logoTxt">
+      Green Alert
+    </div>
+
+
+    <div class="card card-container">
+      <div class="logoImg">
+        <img src=".\Imagens\logo-menu.png" alt="imagemLogo" width=80px>
+      </div>
+      <p id="profile-name" class="profile-name-card"></p>
+      <form method="POST" action="valida.php">
+        <span id="reauth-email" class="reauth-email"></span>
+        <input type="email" name="email" placeholder="Email" required autofocus>
+        <input type="password" name="senha" placeholder="Senha" required>
+        <div id="remember" class="checkbox">
+          <label>
+            <input type="checkbox" value="remember-me"> Lembre-se de mim
+          </label>
         </div>
+        <button type="submit" class="btn btn-success">Entrar</button>
+      </form> <!-- /form -->
+      <a href="#" class="forgot-password">
+        Esqueceu a senha?
+      </a>
+    </div> <!-- /card-container !-->
+  </div>
 
+  <p>
+    <?php
+    //Recuperando o valor da variável global, os erro de login.
+    if (isset($_SESSION['loginErro'])) {
+      echo $_SESSION['loginErro'];
+      unset($_SESSION['loginErro']);
+    } ?>
+  </p>
+  <p>
+    <?php
+    //Recuperando o valor da variável global, deslogado com sucesso.
+    if (isset($_SESSION['logindeslogado'])) {
+      echo $_SESSION['logindeslogado'];
+      unset($_SESSION['logindeslogado']);
+    }
+    ?>
+  </p>
 
-          <div class="card card-container">
-          <div class="logoImg">
-            <img src=".\Imagens\logo-menu.png" alt="imagemLogo" width=80px>
-          </div>
-              <p id="profile-name" class="profile-name-card"></p>
-              <form class="form-signin">
-                  <span id="reauth-email" class="reauth-email"></span>
-                  <input type="text" id="nome" class="form-control" placeholder="Nome" required autofocus>
-                  <input type="password" id="senha" class="form-control" placeholder="Senha" required>
-                  <div id="remember" class="checkbox">
-                      <label>
-                          <input type="checkbox" value="remember-me"> Lembre-se de mim
-                      </label>
-                  </div>
-                  <div class="btnEntrar">
-                      <a href="menu.php"> <!-- Link para pagina "menu.php" !-->
-                      <button type="button" class="btn btn-success">Entrar</button>
-                      </a>       
-                  </div>
-              </form> <!- /form ->
-              <a href="#" class="forgot-password">
-                  Esqueceu a senha?
-              </a>
-          </div> <!-- /card-container !-->
-      </div>  
-  
 
 </body>
+
 </html>
 
 
