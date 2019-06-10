96f632f5ba0c26b12375e2ec706c9833538938fe
lucas <lucasaraujoss95@gmail.com>
2019-06-10 19:00:25 -0300

    quase terminando

--- empresas.php
+++ empresas.php
@@ -128,33 +128,12 @@
                                     <td><?php echo $dado['telefone_empresa']; ?></td>
                                     <td><?php echo $dado['email_empresa']; ?></td>
                                     <td>
-                                        <a href="visualizarEmp.php?id=<?php echo $dado['id_empresa']; ?>">Link</a>
-                                        <button type="button" class="btn btn-primary" >Visualizar</button>
+                                        <a href="visualizarEmp.php?id=<?php echo $dado['id_empresa']; ?>" class="btn btn-primary">View</a>
                                         <button type="button" class="btn btn-success" data-toggle="modal" data-target="#exampleModal" data-whatever="<?php echo $dado['id_empresa']; ?>">Editar</button>
                                         <button type="button" class="btn btn-xs btn-danger">Apagar</button>
                                     </td>
                                 </tr>
 
-                                <!-- Extra large modal -->
-
-
-                                <div id="visualizarEmpresaModal" class="modal fade bd-example-modal-xl" tabindex="-1" role="dialog" aria-labelledby="myExtraLargeModalLabel" aria-hidden="true">
-                                    <div class="modal-dialog modal-xl">
-                                        <div class="modal-content">
-                                            <div class="modal-header">
-                                                <h5 class="modal-title" id="exampleModalScrollableTitle">Modal title</h5>
-                                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                                    <span aria-hidden="true">&times;</span>
-                                                </button>
-                                            </div>
-                                            <div class="modal-body">
-
-
-
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
 
                                 <script>
                                     $(document.ready(function() {
--- visualizarEmp.php
+++ visualizarEmp.php
@@ -3,7 +3,6 @@ include("classes/conexao.php");
 
 
 $id = $_GET['id'];
-echo $id;
 
 //$consulta = "SELECT * FROM cliente_empresa WHERE nomeFantasia_empresa='$nf'";
 $consulta = "SELECT * FROM cliente_empresa WHERE id_empresa ='$id' ";
@@ -13,6 +12,13 @@ $result_consulta =  mysqli_query($conexao, $consulta);
 
 ?>
 
+<script>
+    <!-- Imports -->
+    <
+    link rel = "stylesheet"
+    href = "./css/css-visualizarEmp.css" >
+</script>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -21,6 +27,10 @@ $result_consulta =  mysqli_query($conexao, $consulta);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+
+    <link rel="stylesheet" href="./css/css-visualizarEmp.css">
+    <!-- bootstrap -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 
 <body>
@@ -29,12 +39,123 @@ $result_consulta =  mysqli_query($conexao, $consulta);
     <?php while ($dado = $result_consulta->fetch_array()) { ?>
 
         <div>
-            <?php echo $dado['nomeFantasia_empresa']; ?>
-        </div>
+            <div class="corpo bccu">
 
-        <?php echo $dado['cnpj_empresa']; ?>
-        <?php echo $dado['telefone_empresa']; ?>
-        <?php echo $dado['email_empresa']; ?>
+
+                <div class="topo">
+                    <div>
+                        <h2><i class="material-icons"></i> Detalhes da Empresa</h2>
+                    </div>
+                </div>
+
+                <div class=" flex-center-center">
+                    <form method="POST" action="./classes/Classes-GA/cadastroEmp.php">
+                        <!-- Inicio do formulario -->
+                        <div class="informacoesesquerda">
+                            <div class="form-row">
+                                <div class="form-group col-md-4">
+                                    <label for="cnpj">CNPJ </label><span class="asterisco-ver">*</span>
+                                    <input type="text" class="form-control" name="cnpj" id="cnpjJS" value="<?php echo $dado['cnpj_empresa']; ?>" disabled="disabled">
+                                </div>
+
+                                <div class="form-group col-md-4">
+                                    <label for="numInscricao">Número de Inscrição </label><span class="asterisco-ver">*</span>
+                                    <input type="text" class="form-control" name="numInscricao" id="numInscricaoJS" value="<?php echo $dado['numero_empresa']; ?>" disabled="disabled">
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <label for="nomeEmpresa">Nome da Empresa </label><span class="asterisco-ver">*</span>
+                                <input type="text" class="form-control" name="nomeEmpresa" id="nomeEmpresaJS" value="<?php echo $dado['razaoSocial_empresa']; ?>" disabled="disabled">
+
+                                <label for="nomeFantasia">Nome Fantasia </label><span class="asterisco-ver">*</span>
+                                <input type="text" class="form-control" name="nomeFantasia" id="nomeFantasiaJS" value="<?php echo $dado['nomeFantasia_empresa']; ?>" disabled="disabled">
+                            </div>
+
+                            <div class="form-row">
+                                <div class="form-group col-md-4">
+                                    <label for="descAtv">Principal atividade </label><span class="asterisco-ver">*</span>
+                                    <input type="text" class="form-control" name="descAtv" id="descAtvJS" value="<?php echo $dado['descricao_empresa']; ?>" disabled="disabled">
+                                </div>
+
+                                <div class="form-group col-md-4">
+                                    <label for="telefone">Telefone </label><span class="asterisco-ver">*</span>
+                                    <input type="text" class="form-control ttinput" name="telefone" id="telefoneJS" value="<?php echo $dado['telefone_empresa']; ?>" disabled="disabled">
+                                </div>
+
+                                <div class="form-group col-md-4">
+                                    <label for="telefoneOpc">Telefone Opcional </label>
+                                    <input type="text" class="form-control ttinput" name="telefoneOpc" id="telefoneOpcJS" value="<?php echo $dado['telefoneOpc_empresa']; ?>" disabled="disabled">
+                                </div>
+                            </div>
+
+
+
+                            <div class="form-row">
+                                <div class="form-group col-md-6">
+                                    <label for="responsavel">Responsável </label><span class="asterisco-ver">*</span>
+                                    <input type="text" class="form-control" name="responsavel" id="responsavelJS" value="<?php echo $dado['responsavel_empresa']; ?>" disabled="disabled">
+                                </div>
+
+                                <div class="form-group col-md-6">
+                                    <label for="email">E-mail </label><span class="asterisco-ver">*</span>
+                                    <input type="email" class="form-control" name="email" id="emailJS" value="<?php echo $dado['email_empresa']; ?>" disabled="disabled">
+                                </div>
+                            </div>
+
+
+                            <div class="form-group">
+
+                                <label for="Endereco">Endereço </label><span class="asterisco-ver">*</span>
+                                <input type="text" class="form-control" name="endereco" id="enderecoJS" value="<?php echo $dado['rua_endereco']; ?>" disabled="disabled">
+                            </div>
+
+                            <div class="form-row df">
+
+                                <div class="form-group col-md-2">
+                                    <label for="numero">Número </label><span class="asterisco-ver">*</span>
+                                    <input type="text" class="form-control" name="numero" id="numeroJS" value="<?php echo $dado['numero_endereco']; ?>" disabled="disabled">
+                                </div>
+
+                                <div class="form-group col-md-3">
+                                    <label for="bairro">Bairro </label><span class="asterisco-ver">*</span>
+                                    <input type="text" class="form-control" name="bairro" id="bairroJS" value="<?php echo $dado['bairro_endereco']; ?>" disabled="disabled">
+                                </div>
+
+                                <div class="form-group col-md-3">
+                                    <label for="cidade">Cidade </label><span class="asterisco-ver">*</span>
+                                    <input type="text" class="form-control" name="cidade" id="cidadeJS" value="<?php echo $dado['cidade_endereco']; ?>" disabled="disabled">
+                                </div>
+
+                                <div class="form-group col-md-2">
+                                    <label for="cep">CEP </label><span class="asterisco-ver">*</span>
+                                    <input type="text" class="form-control" name="cep" id="cepJS" value="<?php echo $dado['cep_endereco']; ?>" disabled="disabled">
+                                </div>
+
+                                <div class="form-group col-md-2">
+                                    <label for="uf">UF </label> <span class="asterisco-ver">*</span>
+                                    <input type="text" class="form-control" name="cep" id="cepJS" value="<?php echo $dado['uf_endereco']; ?>" disabled="disabled">
+                                </div>
+                            </div>
+
+
+                            <div class="form-row">
+                                <div class="form-group col-md-4">
+                                    <label for="senha">Senha </label><span class="asterisco-ver">*</span>
+                                    <input type="password" name="senha" class="form-control" id="senhaJS" value="<?php echo $dado['senha_endereco']; ?>" disabled="disabled">
+                                </div>
+                                <div class="form-group col-md-4">
+                                    <label for="confSenha">Confirmar senha </label>
+                                    <input type="password" name="confSenha" class="form-control" id="confSenhaJS" value="<?php echo $dado['confSenha_endereco']; ?>" disabled="disabled">
+                                </div>
+                            </div>
+
+
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
 
 
     <?php }  ?>
